c5b6253c6b141e866fb5aa9d91b1e492a79843a5
ZealanTannerMtech <zealan.tanner0869@stu.mtec.edu>
2025-10-24 12:40:12 -0600

    initial2

---
+++ In Class Assignment Variables/main.js
@@ -0,0 +1,17 @@
+const stringConst = "apple";
+let stringLet = "orange";
+var stringVar = "zebra";
+
+console.log(stringConst)
+console.log(stringLet)
+console.log(stringVar)
+
+
+
+const numberConst = 1;
+let numberLet = 2;
+var numberVar = 3;
+
+console.log(numberConst)
+console.log(numberLet)
+console.log(numberVar)
