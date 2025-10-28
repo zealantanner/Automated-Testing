99e2a9474ec47d4e2248cb3c11bdc1cc7a2a8b53
ZealanTannerMtech <zealan.tanner0869@stu.mtec.edu>
2025-10-28 13:03:03 -0600

    did conditional statements assignment

---
+++ Conditional Statements Assignment.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let rand = Math.floor(Math.random() * 10);
+
+console.log(rand)
+
+// if(rand == 1) {
+//     console.log(rand + " is 1")
+// }
+
+// if(rand == 2) {
+//     console.log(rand + " is 2")
+// } else {
+//     console.log(rand + " is not 2")
+// }
+
+// if(rand == 3) {
+//     console.log(rand + " is 3")
+// } else if(rand == 4) {
+//     console.log(rand + " is 4")
+// } else {
+//     console.log(rand + " is not 3 or 4")
+// }
+
+
+if(rand == 5) {
+    console.log(rand + " is 5")
+} else if(rand == 6) {
+    console.log(rand + " is 6")
+} else if(rand == 7) {
+    console.log(rand + " is 7")
+} else {
+    console.log(rand + " is not 5, 6, or 7")
+}
---
+++ thing.js
@@ -0,0 +1,6 @@
+"use strict";
+let mistypeVariable;
+
+mistypeVarible = 17;
+console.log(mistypeVarible)
+console.log(mistypeVariable)
